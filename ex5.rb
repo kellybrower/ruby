8b55e8a106b6f8aa6cdb652d66a7eb7532ad9625
Kelly Brower <kbrower77@gmail.com>
2018-06-01 23:45:53 -0700

    Add files via upload

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Kelly R. Brower'
+my_age = 28 # veritas in 2018
+my_height = 72 # inches ok 71.5 jeesh
+my_weight = 180 # according to my drivers lis
+my_eyes = 'Green/Hazel'
+my_teeth = 'Whiteish'
+my_hair = 'Strawberry Blonde'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually, we don't really know how heavy he is."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+#this line is tricky apparently
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I should get #{my_age + my_height + my_weight}."
