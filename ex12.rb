1af319eef0593db486abd9706e3ee406fcf6ed41
Kelly Brower <kbrower77@gmail.com>
2018-06-02 23:32:15 -0700

    Update ex12.rb

--- ex12.rb
+++ ex12.rb
@@ -2,7 +2,7 @@ print "Give me a number: "
 number = gets.chomp.to_i
 
 bigger = number * 100
-puts "A biggr number is #{bigger}."
+puts "A bigger number is #{bigger}."
 
 print "Give me another number: "
 another = gets.chomp
